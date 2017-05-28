40515da78997e6f10958f9b13cd67ff0e75b1253
MohanaKrishnan <555mohann@gmail.com>
2017-05-28 19:11:14 +0530

    Basic Load

---
+++ Main.js
@@ -0,0 +1,28 @@
+define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
+    'http://worldwindserver.net/webworldwind/examples/LayerManager.js'],
+    function (ww,LayerManager) {
+        "use strict";
+
+        WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
+
+        var wwd = new WorldWind.WorldWindow("canvasOne");
+
+        var layers = [
+            {layer: new WorldWind.BMNGLayer(), enabled: true},
+            {layer: new WorldWind.BMNGLandsatLayer(), enabled: false},
+            {layer: new WorldWind.BingAerialLayer(null), enabled: false},
+            {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: true},
+            {layer: new WorldWind.BingRoadsLayer(null), enabled: false},
+            {layer: new WorldWind.CompassLayer(), enabled: true},
+            {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
+            {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
+        ];
+
+        for (var l = 0; l < layers.length; l++) {
+            layers[l].layer.enabled = layers[l].enabled;
+            wwd.addLayer(layers[l].layer);
+        }
+
+        // Create a layer manager for controlling layer visibility.
+        var layerManger = new LayerManager(wwd);
+    });
